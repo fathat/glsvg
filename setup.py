331a9fa61203640205f9af5d98ebb5d12c67a605
Ian Overgard <ian.overgard@gmail.com>
2013-02-18 00:16:44 -0700

    Created a setup.py file

--- setup.py
+++ setup.py
@@ -1,1 +1,24 @@
-# TODO: make setup.py
+
+from distutils.core import setup
+setup(
+    name = "glsvg",
+    packages = ["glsvg"],
+    version = "0.1",
+    description = "OpenGL SVG Renderer",
+    author = "Ian Overgard",
+    author_email = "ian.overgard@gmail.com",
+    url = "http://github.com/fathat/glsvg",
+    download_url = "http://github.com/fathat/glsvg",
+    keywords = ["opengl", "svg", "xml"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Games/Entertainment",
+        "Topic :: Multimedia :: Graphics"
+        ]
+)
